2afda0a269ac1ad9fc71beb7efd31154f74f0e94
LuizFilippe1995 <53869523+LuizFilippe1995@users.noreply.github.com>
2021-06-16 20:29:55 -0300

    Update bank_ewa_strategy.py

--- banksim/strategies/bank_ewa_strategy.py
+++ banksim/strategies/bank_ewa_strategy.py
@@ -1,10 +1,10 @@
 class BankEWAStrategy:
     # capital ratio (capital / assets)
-    numberAlphaOptions = 10
+    numberAlphaOptions = 30
     # liquidity ratio(liquid assets / deposits)
     numberBetaOptions = 20
     # risk appetite (High Risk Corporate Client/ Low Risk Corporate Client)
-    numberGammaOptions = 20
+    numberGammaOptions = 30
 
     def __init__(self, alpha_index_option=0, beta_index_option=0, gamma_index_option=0):
         self.alphaIndex = alpha_index_option
@@ -12,7 +12,8 @@ class BankEWAStrategy:
         self.gammaIndex = gamma_index_option
         self.strategyProfit = self.strategyProfitPercentage = self.strategyProfitPercentageDamped = 0
         self.A = self.P = self.F = 0
-     
+    
+    
     def get_alpha_value(self):
         return (self.alphaIndex + 1) / 100 
     
@@ -20,11 +21,12 @@ class BankEWAStrategy:
         return  (self.betaIndex + 1) / 100 
     
     def get_gamma_value(self):
-        return (self.gammaIndex + 3) / 100
+        return (self.gammaIndex + 1) / 100
         
     def __eq__(self, other):
         if isinstance(other, self.__class__):
-            return self.alphaIndex == other.alphaIndex and self.betaIndex == other.betaIndex and self.gammaIndex == other.gammaIndex
+            return self.alphaIndex == other.alphaIndex and self.betaIndex == other.betaIndex and \
+        self.gammaIndex == other.gammaIndex
         return False
     
     def reset(self):
@@ -33,5 +35,7 @@ class BankEWAStrategy:
 
     @classmethod
     def bank_ewa_strategy_list(cls):
-        return [BankEWAStrategy(a, b, c) for a in range(cls.numberAlphaOptions) for b in range(cls.numberBetaOptions) for c in range(cls.numberGammaOptions)]
+        return [BankEWAStrategy(a, b, c) for a in range(cls.numberAlphaOptions) for b in \
+                range(cls.numberBetaOptions) for c in range(cls.numberGammaOptions)]
+
                 
