bb3e3ed65c1d2c47edf451dfe5ff7915d37b2e44
LuizFilippe1995 <53869523+LuizFilippe1995@users.noreply.github.com>
2021-06-16 20:32:17 -0300

    Update depositor_ewa_strategy.py

--- banksim/strategies/depositor_ewa_strategy.py
+++ banksim/strategies/depositor_ewa_strategy.py
@@ -2,14 +2,17 @@ import numpy as np
 
 
 class DepositorEWAStrategy:
-    numberAlphaOptions = 10
+    numberAlphaOptions = 10 #30
 
     def __init__(self, alpha_index_option=0):
         self.alphaIndex = alpha_index_option
-        self.strategyProfit = self.amountEarlyWithdraw = self.amountFinalWithdraw = 0
-        self.insolvencyCounter = self.finalConsumption = 0
+        self.strategyProfit = 0
+        self.amountEarlyWithdraw = 0
+        self.amountFinalWithdraw = 0
+        self.insolvencyCounter = 0
+        self.finalConsumption = 0
         self.A = self.P = self.F = 0
-
+    
     def get_alpha_value(self):
         return (self.alphaIndex + 1) / 100
 
@@ -17,7 +20,7 @@ class DepositorEWAStrategy:
         if isinstance(other, self.__class__):
             return self.alphaIndex == other.alphaIndex
         return False
-
+    
     def reset(self):
         self.strategyProfit = self.amountEarlyWithdraw = self.amountFinalWithdraw = 0
         self.insolvencyCounter = self.finalConsumption = 0
@@ -26,3 +29,4 @@ class DepositorEWAStrategy:
     @classmethod
     def depositor_ewa_strategy_list(cls):
         return np.array([DepositorEWAStrategy(a) for a in range(cls.numberAlphaOptions)], dtype=DepositorEWAStrategy)
+                        
